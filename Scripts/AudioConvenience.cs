e1ddfa70cf44c44fadb46926294bbecd5e2f85f5
Tiko-M <73656614+Tiko-M@users.noreply.github.com>
2025-09-06 00:03:18 +0700

    Split file

--- Scripts/AudioConvenience.cs
+++
@@ -1,7 +0,0 @@
-public static class AudioConvenience
-{
-    public static Tiko.AudioSystem.AudioHandle PlaySFX(this Tiko.AudioSystem.AudioManager m, EAudio e)
-        => m.PlaySFX(e.ToString());
-    public static void PlayMusic(this Tiko.AudioSystem.AudioManager m, EAudio e)
-        => m.PlayMusic(e.ToString());
-}
